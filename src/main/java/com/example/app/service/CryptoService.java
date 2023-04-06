69c2efa63fc3d0e25e6d9b301abba3aed832adce
dmitrykap1an <dk475641@gmail.com>
2023-04-07 00:05:25 +0300

    crypto methods have been moved from AuthService.java to a separate CryptoService.java

--- src/main/java/com/example/app/service/CryptoService.java
+++ src/main/java/com/example/app/service/CryptoService.java
@@ -1,19 +1,49 @@
-package com.example.app.utils.security;
+package com.example.app.service;
 
+import com.example.app.repository.SaltRepository;
+import com.example.app.utils.exceptions.UserNotFoundException;
+import com.example.app.utils.model.entities.Salt;
 import com.example.app.utils.model.entities.User;
 import io.jsonwebtoken.io.Encoders;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Random;
 
-@Component
 @Slf4j
-public class Crypto {
+@Service
+public class CryptoService {
+
+    SaltRepository saltRepository;
+
+    public CryptoService(SaltRepository saltRepository){
+        this.saltRepository = saltRepository;
+    }
+
+    public String getHash(@NonNull User user) throws UserNotFoundException {
+        Salt salts = saltRepository.getSaltByOwner(user.getLogin());
+        if (salts == null) throw new UserNotFoundException("Пользователь с данным логином и паролем не найден");
+        String salt1 = salts.getSalt1();
+        String salt2 = salts.getSalt2();
+        String hash = md5(salt1 + user.getPassword() + salt2);
+        return md5(hash);
+    }
+
+
+    public String doHash(@NonNull User user) {
+        String salt1 = generateSalt(20);
+        String salt2 = generateSalt(20);
+        Salt salt = new Salt(salt1, salt2, user.getLogin());
+        saltRepository.save(salt);
+        String hash = md5(salt1 + user.getPassword() + salt2);
+        return md5(hash);
+    }
+
 
     public String md5(String password) {
         try {
