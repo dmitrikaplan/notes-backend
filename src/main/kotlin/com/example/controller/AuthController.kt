2f9e7d6b1aad0c6fff53db4b3a919f1faed27cd3
dmitrykap1an <dk475641@gmail.com>
2023-08-05 14:25:32 +0300

    changed the format of the server response to a self-generated json

--- src/main/kotlin/com/example/controller/AuthController.kt
+++ src/main/kotlin/com/example/controller/AuthController.kt
@@ -1,75 +1,120 @@
 package com.example.controller
 
 import com.example.service.AuthService
+import com.example.utils.dto.UserWithoutLoginOrEmail
 import com.example.utils.exceptions.*
-import com.example.utils.model.JwtResponse
-import com.example.utils.model.entities.User
+import com.example.utils.dto.entities.User
+import com.example.utils.dto.responses.MessageResponse
 import jakarta.mail.MessagingException
+import jakarta.validation.Valid
+import org.hibernate.validator.constraints.Length
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 
-@RestController
+@Validated
+@Controller
 @RequestMapping("/api/v1/auth/")
 class AuthController (
-    private val authService: AuthService
+    private val authService: AuthService,
 ){
 
+    private val log = LoggerFactory.getLogger(AuthController::class.java)
+
     @PostMapping("registration")
-    @ResponseBody // TODO: 7/24/23 добавить валидационный слой,
-    //  который будет проверять отсутсвие activation и activationCode
-    fun registration(@RequestBody(required = true) user: User): ResponseEntity<String> {
+    fun registration(@RequestBody(required = true) @Valid user: User): ResponseEntity<String> {
         return try {
             authService.registration(user)
-            ResponseEntity.status(HttpStatus.OK).body("Код подтверждения отправлен вам на почту")
-        } catch (e: UserAlreadyRegisteredException) {
-            ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.message)
-        } catch (e: NotValidUserException) {
-            ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Некорректные(й) логин и/или пароль, и/или почта")
-        } catch (e: MessagingException) {
+            val messageResponse = MessageResponse("Код подтверждения отправлен вам на почту")
+            log.info("Код подтверждения для пользователя ${user.getLogin()!!.uppercase()} отправлен на почту")
+            ResponseEntity.status(HttpStatus.OK).body(messageResponse.toJson())
+        } 
+        catch (e: UserAlreadyRegisteredException) {
+            val messageResponse = MessageResponse(e.message)
+            ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(messageResponse.toJson())
+        }
+        catch (e: NotValidUserException) {
+            log.debug(e.message)
+            val messageResponse = MessageResponse(e.message)
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(messageResponse.toJson())
+        } 
+        catch (e: MessagingException) {
+            log.debug("Ошибка отправки сообщения на почту")
+            val messageResponse = MessageResponse("Ошибка отправки сообщения на почту. Повторите попытку позже")
             ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT)
-                .body("Ошибка отправки сообщения на почту. Повторите попытку позже")
+                .body(messageResponse.toJson())
+        } 
+        catch (e: UnexpectedActivationCodeException){
+            log.debug(e.message)
+            val messageResponse = MessageResponse(e.message)
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(messageResponse.toJson())
+        } 
+        catch (e: UnexpectedActivatedException){
+            log.debug(e.message)
+            val messageResponse = MessageResponse(e.message)
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(messageResponse.toJson())
         }
     }
 
     @PostMapping("login")
     fun login(
-        @RequestBody(required = true) user: User
-    ): ResponseEntity<JwtResponse> {
+        @RequestBody(required = true) @Valid userWithoutLoginOrEmail: UserWithoutLoginOrEmail
+    ): ResponseEntity<String> {
         return try {
-            val jwtResponse = authService.login(user)
-            ResponseEntity.status(HttpStatus.OK).body(jwtResponse)
-        } catch (e: UserNotFoundException) {
-            val jwtResponse = JwtResponse(e.message, null)
-            ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(jwtResponse)
-        } catch (e: NotValidUserException) {
-            val jwtResponse = JwtResponse(e.message, null)
-            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(jwtResponse)
+            val jwtResponse = authService.login(userWithoutLoginOrEmail)
+            ResponseEntity.status(HttpStatus.OK).body(jwtResponse.toJson())
+        } 
+        catch (e: UserNotFoundException) {
+            log.debug(e.message)
+            val messageResponse = MessageResponse(e.message)
+            ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(messageResponse.toJson())
+        } 
+        catch (e: NotValidUserException) {
+            log.debug(e.message)
+            val messageResponse = MessageResponse(e.message)
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(messageResponse.toJson())
+        }
+        catch (e: UnexpectedActivationCodeException){
+            log.debug(e.message)
+            val messageResponse = MessageResponse(e.message)
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(messageResponse.toJson())
+        }
+        catch (e: UnexpectedActivatedException){
+            log.debug(e.message)
+            val messageResponse = MessageResponse(e.message)
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(messageResponse.toJson())
         }
     }
 
     @GetMapping("activation/{code}")
-    fun activateAccount(@PathVariable("code", required = true) code: String): String {
+    fun activateAccount(@PathVariable("code", required = true) @Length(min = 1) code: String): ResponseEntity<String>{
         return try {
             authService.activateAccount(code)
-            "Аккаунт успешно активирован"
+            ResponseEntity.status(HttpStatus.OK).body("Аккаунт успешно активирован")
         } catch (e: NotFoundUserByActivationCode) {
-            "Аккаунт уже активирован"
+            ResponseEntity.status(HttpStatus.OK).body("Аккаунт уже активирован")
         }
     }
 
     @PostMapping("recovery")
-    fun passwordRecovery(@RequestBody(required = true) user: User): ResponseEntity<String> {
+    fun passwordRecovery(@RequestBody(required = true) @Valid userWithoutLoginOrEmail: UserWithoutLoginOrEmail): ResponseEntity<String> {
         return try {
-            authService.passwordRecovery(user)
-            ResponseEntity.status(HttpStatus.OK).body("Код восстановления отправлен Вам на почту")
+            authService.passwordRecovery(userWithoutLoginOrEmail)
+            val messageResponse = MessageResponse("Код восстановления отправлен Вам на почту")
+            ResponseEntity.status(HttpStatus.OK).body(messageResponse.toJson())
         } catch (e: NotValidEmailException) {
-            ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Неверный формат почты")
+            val messageResponse = MessageResponse("Неверный формат почты")
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(messageResponse.toJson())
         } catch (e: UserNotFoundException) {
-            ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Пользователь с такой почтой не найден")
+            val messageResponse = MessageResponse("Пользователь с такой почтой не найден")
+            ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(messageResponse.toJson())
         } catch (e: MessagingException) {
+            val messageResponse = MessageResponse("Ошибка отправки сообщения на почту. Повторите попытку позже")
             ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT)
-                .body("Ошибка отправки сообщения на почту. Повторите попытку позже")
+                .body(messageResponse.toJson()  )
         }
     }
 }
--- src/main/kotlin/com/example/controller/NoteController.kt
+++ src/main/kotlin/com/example/controller/NoteController.kt
@@ -1,36 +1,63 @@
 package com.example.controller
 
 import com.example.service.NoteService
-import com.example.utils.model.entities.Note
+import com.example.utils.exceptions.notesExceptions.NoteCannotBeAddedException
+import com.example.utils.exceptions.notesExceptions.NoteCannotUpdatedException
+import com.example.utils.dto.entities.Note
+import com.example.utils.dto.responses.MessageResponse
+import jakarta.validation.ConstraintViolationException
+import jakarta.validation.constraints.Min
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.*
 
-@RestController
+@Validated
+@Controller
 @RequestMapping("/api/v1/notes")
-class NoteController (
+class NoteController(
     private val noteService: NoteService
-){
+) {
     @PostMapping("add")
-    fun addNotes(@RequestBody note: Note?): ResponseEntity<Note> {
-        val note1 = noteService.addNote(note!!)
-        return ResponseEntity.status(HttpStatus.OK).body(note1)
+    fun addNotes(@RequestBody(required = true) note: Note): ResponseEntity<String> {
+        return try {
+            val note1 = noteService.addNote(note)
+            ResponseEntity.status(HttpStatus.OK).body(note1.toJson())
+        } catch (e: NoteCannotBeAddedException) {
+            val messageResponse = MessageResponse(e.message)
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(messageResponse.toJson())
+        }
     }
 
     @PostMapping("update")
-    fun updateNote(@RequestBody note: Note?): ResponseEntity<Note> {
-        val note1 = noteService.updateNote(note!!)
-        return ResponseEntity.status(HttpStatus.OK).body(note1)
+    fun updateNote(@RequestBody(required = true) note: Note): ResponseEntity<String> {
+        return try {
+            val note1 = noteService.updateNote(note)
+            ResponseEntity.status(HttpStatus.OK).body(note1.toJson())
+        } catch (e: NoteCannotUpdatedException) {
+            val messageResponse = MessageResponse(e.message)
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(messageResponse.toJson())
+        }
+
     }
 
     @DeleteMapping("delete")
-    fun deleteNote(@RequestBody id: Long?): ResponseEntity<String> {
-        noteService.deleteNote(id!!)
-        return ResponseEntity.status(HttpStatus.OK).body("Заметка успешно удалена")
+    fun deleteNote(@RequestBody(required = true) @Min(0) id: Long): ResponseEntity<String> {
+        return try {
+            noteService.deleteNote(id)
+            val messageResponse = MessageResponse("Заметка успешно удалена")
+            ResponseEntity.status(HttpStatus.OK).body(messageResponse.toJson())
+        }
+        catch (e: ConstraintViolationException){
+            val messageResponse = MessageResponse("id должен быть больше 0")
+            ResponseEntity.status(HttpStatus.BAD_REQUEST).body(messageResponse.toJson())
+        }
     }
 
-    @GetMapping("get")
-    fun getNotes(): ResponseEntity<List<Note>>{
+    @GetMapping("get-all")
+    @ResponseBody
+    fun getNotes(): ResponseEntity<List<Note>> {
         val notes = noteService.allNotes()
         return ResponseEntity.status(HttpStatus.OK).body(notes)
     }
