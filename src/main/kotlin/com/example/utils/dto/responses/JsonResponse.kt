ccc847e7d12e603eb9e16bb9860f0400ca714770
dmitrykap1an <dk475641@gmail.com>
2023-08-05 16:39:57 +0300

    added a common parent class

--- src/main/kotlin/com/example/utils/dto/responses/JsonResponse.kt
+++ src/main/kotlin/com/example/utils/dto/responses/JsonResponse.kt
@@ -5,11 +5,7 @@ import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
 @Serializable
-data class JwtResponse(
-    private val accessToken: String,
-    private val refreshToken: String,
-): JsonResponse()
-
-    //private val type = "Bearer"
-
-
+sealed class JsonResponse {
+    fun toJson() =
+        Json.encodeToString(this)
+}
--- src/main/kotlin/com/example/utils/dto/responses/MessageResponse.kt
+++ src/main/kotlin/com/example/utils/dto/responses/MessageResponse.kt
@@ -1,13 +1,7 @@
 package com.example.utils.dto.responses
 
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
 
 
 @Serializable
-data class MessageResponse(private val message: String?){
-
-    fun toJson() =
-        Json.encodeToString(this)
-}
+data class MessageResponse(private val message: String?): JsonResponse()
