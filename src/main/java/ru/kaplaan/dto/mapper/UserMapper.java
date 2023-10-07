69e5b2dbc9a12c35ab063a66a57fcfaab9a9b532
dmitrykap1an <dk475641@gmail.com>
2023-10-07 20:37:34 +0300

    added ignoring properties

--- src/main/java/ru/kaplaan/dto/mapper/UserMapper.java
+++ src/main/java/ru/kaplaan/dto/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package ru.kaplaan.dto.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.kaplaan.domain.entity.User;
 import ru.kaplaan.web.dto.user.UserDto;
 
@@ -10,6 +11,9 @@ public interface UserMapper {
 
     UserDto toDto(User user);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "activationCode", ignore = true)
+    @Mapping(target = "activated", ignore = true)
     User toEntity(UserDto userDto);
 
 }
