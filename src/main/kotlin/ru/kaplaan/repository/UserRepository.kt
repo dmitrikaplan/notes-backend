3aaa536324f6b98d2f2627550fc2eca33f247357
dmitrykap1an <dk475641@gmail.com>
2023-10-25 04:50:44 +0300

    renamed methods

--- src/main/kotlin/ru/kaplaan/repository/UserRepository.kt
+++ src/main/kotlin/ru/kaplaan/repository/UserRepository.kt
@@ -6,29 +6,28 @@ import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
-import ru.kaplaan.domain.entity.User
+import ru.kaplaan.domain.entity.user.User
 
 @Transactional
 @Repository
 interface UserRepository : JpaRepository<User, Long> {
 
-    fun existsUserByLogin(login: String): Boolean
+    fun existsUserByUsername(username: String): Boolean
 
     fun existsUserByActivationCode(activationCode: String): Boolean
 
-    fun existsUserByLoginOrEmailAndActivated(login: String, email: String, activated: Boolean): Boolean
+    fun existsUserByUsernameOrEmailAndActivated(username: String, email: String, activated: Boolean): Boolean
 
-    fun existsUserByLoginAndPasswordAndEmailAndActivated(
-        login: String,
+    fun existsUserByUsernameAndPasswordAndEmailAndActivated(
+        username: String,
         password: String,
         email: String,
         activated: Boolean
     ): Boolean
 
-    fun getUserByEmail(email: String): User?
+    fun findByEmail(email: String): User?
 
-    fun getUserByLogin(login: String): User?
-
+    fun findByUsername(username: String): User?
 
     fun getUserByActivationCode(code: String): User?
 
@@ -38,4 +37,5 @@ interface UserRepository : JpaRepository<User, Long> {
         @Param("activated") activated: Boolean,
         @Param("activationCode") activationCode: String
     )
+
 }
