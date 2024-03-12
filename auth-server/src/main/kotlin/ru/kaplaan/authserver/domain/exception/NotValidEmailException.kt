cce1dac255b879ee63678f6c709e8c2cebf74a3f
dmitrykap1an <dk475641@gmail.com>
2024-03-12 16:30:48 +0300

    outdated exceptions have been removed

--- auth-server/src/main/kotlin/ru/kaplaan/authserver/domain/exception/NotValidEmailException.kt
+++
@@ -1,6 +0,0 @@
-package ru.kaplaan.authserver.domain.exception
-
-class NotValidEmailException : RuntimeException(){
-    override val message: String
-        get() = "Неверный формат почты"
-}
--- auth-server/src/main/kotlin/ru/kaplaan/authserver/domain/exception/NotValidUserException.kt
+++
@@ -1,6 +0,0 @@
-package ru.kaplaan.authserver.domain.exception
-
-class NotValidUserException : RuntimeException(){
-    override val message: String
-        get() = "Некорректные(й) логин и/или пароль, и/или почта"
-}
